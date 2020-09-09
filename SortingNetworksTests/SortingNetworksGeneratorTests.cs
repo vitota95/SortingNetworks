c7ad7d87d32a69614868e071bac461cb328a9e57
vitota95 <javigongar95@gmail.com>
2020-09-09 12:14:06 +0200

    Add sorting newor generator tests

---
+++ SortingNetworksTests/SortingNetworksGeneratorTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingNetworks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingNetworksTests
+{
+    [TestClass]
+    public class SortingNetworksGeneratorTests
+    {
+        [TestMethod()]
+        public void Generate_WithRedundantComparator_NotAddNewNet()
+        {
+            // Arrange
+            short size = 2;
+            var comparators = new Comparator[] { new Comparator(0, 1), new Comparator(0, 1) };
+            var nets = new ComparatorNetwork[] { new ComparatorNetwork(size, comparators) };
+            var generator = new SortingNetworksGenerator();
+
+            // Act
+            var result = generator.Generate(nets, comparators);
+
+            // Assert
+            Assert.AreEqual(1, result.Length);
+        }
+        
+        [TestMethod()]
+        public void Generate_WithNonRedundantComparator_AddsNewNet()
+        {
+            // Arrange
+            short size = 3;
+            var c1 = new Comparator[1] { new Comparator(0, 1) };
+            var c2 = new Comparator[2] { new Comparator(0, 1), new Comparator(0, 2) };
+            var nets = new ComparatorNetwork[] { new ComparatorNetwork(size, c1) };
+            var generator = new SortingNetworksGenerator();
+
+            // Act
+            var result = generator.Generate(nets, c2);
+
+            // Assert
+            Assert.AreEqual(2, result.Length);
+        }
+    }
+}
