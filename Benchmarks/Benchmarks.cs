868749f680eb4366c14b22668932eb76e738b897
vitota95 <javigongar95@gmail.com>
2020-10-05 16:45:39 +0200

    add benchmarks project

---
+++ Benchmarks/Benchmarks.cs
@@ -0,0 +1,27 @@
+﻿namespace Benchmarks
+{
+    using System;
+
+    using BenchmarkDotNet;
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Running;
+
+    using SortingNetworks;
+
+    public class Benchmarks
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Benchmarks>();
+        }
+
+        [Benchmark]
+        [Arguments(6, 12)]
+        [Arguments(7, 16)]
+        public void SingleCore(int inputs, int comparators)
+        {
+            var args = $@"{inputs} {comparators} benchmarks.log".Split(' ');
+            Program.Main(args);
+        }
+    }
+}
